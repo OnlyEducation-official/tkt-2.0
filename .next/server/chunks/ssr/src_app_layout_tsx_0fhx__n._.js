9bb469f464adc67ed46c5b0a70937cfd3d6ea027
Ali Shaikh <qasimshaikh730@gmail.com>
2026-05-12 15:12:37 +0530

    remove .next folder from there

--- .next/server/chunks/ssr/src_app_layout_tsx_0fhx__n._.js
+++
@@ -1,3 +0,0 @@
-module.exports=[27572,a=>{"use strict";var b=a.i(7997);a.s(["default",0,function({children:a}){return(0,b.jsx)("html",{lang:"en",className:"dark",children:(0,b.jsx)("body",{children:a})})},"metadata",0,{title:"The Kerala Table | Authentic Kerala Dining in Navi Mumbai",description:"A premium Kerala-inspired South Indian restaurant experience in Vashi, Navi Mumbai, rooted in banana-leaf dining, warm hospitality, and coastal flavors."}])},50645,a=>{a.n(a.i(27572))}];
-
-//# sourceMappingURL=src_app_layout_tsx_0fhx__n._.js.map
